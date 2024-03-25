6f283ed55a232929fb16d6255aa18d442918bfd2
Flávio Carrara <flavio@carrara.us>
2024-03-25 09:20:35 -0300

    All plain responses, no ornaments.

--- utils/log.go
+++ utils/log.go
@@ -3,17 +3,17 @@ package utils
 import "fmt"
 
 func LogSuccess(message string) {
-	fmt.Printf("\033[1;32m%s\033[0m\n", message)
+	fmt.Printf("%s", message)
 }
 
 func LogError(message string) {
-	fmt.Printf("\033[1;31m%s\033[0m\n", message)
+	fmt.Printf("%s", message)
 }
 
 func LogInfo(message string) {
-	fmt.Printf("\n\033[1;36m%s\033[0m%s\n", ">>  ", message)
+	fmt.Printf("%s", message)
 }
 
 func LogExplanation(message string) {
-	fmt.Printf("\n\033[1;33m%s\033[0m\n\n%s\n", "Explanation >>  ", message)
+	fmt.Printf("%s", message)
 }
