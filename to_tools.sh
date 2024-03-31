f24cc090c8455a7288d4c240e31a3b79f5f6c64e
fde-capu <flavio@carrara.us>
2024-03-31 20:03:54 -0300

    Now to_tools flow.

--- to_tools.sh
+++ to_tools.sh
@@ -1,4 +1,3 @@
-cp -fluv --preserve=all ./bin/navi ~/_/toolz/_
-mkdir -p ~/.vim/pack/my/start/navi/plugin
-cp -fluv --preserve=all ./navi.vim ~/.vim/pack/my/start/navi/plugin/navi.vim
-echo "Don't froget to re-source navi.vim"
+cp -fluv --preserve=all ./bin/navi $TOOLZ_DIR/_
+cp -fluv --preserve=all ./navi.vim ~/.vim
+echo "Don't froget to \`:source ~/.vim/navi.vim\`."
