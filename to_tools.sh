af5d428a2a4946a9cb1721e5fea7c9132df7d945
fde-capu <flavio@carrara.us>
2024-04-01 20:57:57 -0300

    Set PLUGINDIR in to_tools.sh

--- to_tools.sh
+++ to_tools.sh
@@ -1,6 +1,7 @@
 make gomod
 make
+PLUGINDIR=~/.vim/after/plugin
 cp -fluv --preserve=all ./bin/navi $TOOLZ_DIR/_
 cp -fluv --preserve=all ./.navi_env $TOOLZ_DIR/
-cp -fluv --preserve=all ./navi.vim ~/.vim
-echo "Don't froget to \`:source ~/.vim/navi.vim\`."
+cp -fluv --preserve=all ./navi.vim $PLUGINDIR
+echo "Don't froget to \`:source $PLUGINDIR/navi.vim\`."
