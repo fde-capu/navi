77d8fb8422db8537ff29a6b1136e0b400750a507
fde-capu <flavio@carrara.us>
2024-04-17 12:34:22 -0300

    typo

--- to_tools.sh
+++ to_tools.sh
@@ -4,4 +4,4 @@ PLUGINDIR=~/.vim/after/plugin
 cp -fluv --preserve=all ./bin/navi $TOOLZ_DIR/_
 cp -fluv --preserve=all ./.navi_env $TOOLZ_DIR/
 cp -fluv --preserve=all ./navi.vim $PLUGINDIR
-echo "Don't froget to \`:source $PLUGINDIR/navi.vim\`."
+echo "Don't forget to \`:source $PLUGINDIR/navi.vim\`."
